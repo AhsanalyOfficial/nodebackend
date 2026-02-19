8809bf586f1f8efa7a0fb5227015c2da7ae2352d
AhsanAliOfficial <ahsanalyofficial@gmail.com>
2026-02-19 23:24:22 +0500

    fix: set default PORT to 3000 and simplify server listen logic

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-const PORT = process.env.PORT;
+const PORT = 3000;
 app.get("", (res, req) => {
   req.send("node.js backend is running");
 });
 
-app.listen(PORT || 3000, () => console.log(`Server run on this ${PORT}`));
+app.listen(PORT, () => console.log(`Server run on this ${PORT}`));
