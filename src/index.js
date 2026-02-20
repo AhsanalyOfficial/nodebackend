19049d28033ef0c601b85eca402230dd05a51f41
AhsanalyOfficial <ahsanalyofficial@gmail.com>
2026-02-20 23:36:04 +0500

    ddddd

--- src/index.js
+++ src/index.js
@@ -7,5 +7,8 @@ const PORT = process.env.PORT || 8080;
 app.get("", (req, res) => {
   res.send("shahid axha bacha ni hy.");
 });
+app.get("login", (req, res) => {
+  res.send("asad bht harami hy.");
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
