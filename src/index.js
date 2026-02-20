a066900b26baf6cf4503ff3956392340c3cce263
AhsanalyOfficial <ahsanalyofficial@gmail.com>
2026-02-20 20:36:40 +0500

    minor changes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const dotenv = require("dotenv");
 const app = express();
 dotenv.config();
 
-const PORT = 3000;
-app.get("", (res, req) => {
-  req.send("node.js backend is running");
+const PORT = process.env.PORT || 8080;
+app.get("", (req, res) => {
+  res.send("node.js backend is running");
 });
 
-app.listen(PORT, "0.0.0.0", () => console.log(`Server run on this ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
