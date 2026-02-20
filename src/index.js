c0849485f0bc27c4328403faaa558ed9a6ef3f39
AhsanalyOfficial <ahsanalyofficial@gmail.com>
2026-02-21 00:11:20 +0500

    ytttttt

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080;
 app.get("", (req, res) => {
   res.send("shahid axha bacha ni hy.");
 });
-app.get("login", (req, res) => {
+app.get("/login", (req, res) => {
   res.send("asad bht harami hy.");
 });
 
